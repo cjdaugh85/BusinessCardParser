d97ce41e1f34eda0c25c6a0cfae888e8e467f67b
Chris Daugherty <chrisdaugherty@Eyez.local>
2018-12-20 15:23:25 -0500

    fixing

--- PodcastManager/src/public/app.js
+++
@@ -1,5 +0,0 @@
-const angular = require('angular');
-
-angular.module("app", []);
-
-require('./components/home/HomeController');
--- PodcastManager/src/public/components/home/HomeController.js
+++
@@ -1,8 +0,0 @@
-const angular = require('angular');
-
-const HomeController = () => {
-    this.welcome = "Hello World! We're using Angular!";
-};
-
-angular.module("app").controller('HomeController', HomeController);
-
